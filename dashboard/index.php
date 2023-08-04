68e6a2952a7f488aae3eed3782219cc19286f18f
fikariahana <fikaraihana15@gmail.com>
2023-08-04 09:14:27 +0700

    first commit

---
+++ dashboard/index.php
@@ -0,0 +1,12 @@
+<?php include_once('header.php'); ?>
+
+<div class="row">
+<div class="col-lg-12">
+	<h1>Dashboard</h1>
+	<p>Selamat Datang <code><?= $_SESSION['user'] ?></code> di website Rumah Sakit | Repost by <a href='https://stokcoding.com/' title='StokCoding.com' target='_blank'>StokCoding.com</a>
+	</p>
+</div>
+</div>
+
+
+<?php include_once('footer.php'); ?>
